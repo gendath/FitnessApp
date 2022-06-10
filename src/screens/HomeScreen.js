6f25302e95089827b984ddc13c1420974596193c
gendath <sellwithperry@gmail.com>
2022-06-10 09:36:50 -0700

    migrate to vscode

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+  return <Text style={styles.text}>John is now King Asshole</Text>;
 };
 
 const styles = StyleSheet.create({
